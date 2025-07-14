e9afea67e43a8803d2a7d591825e452e2a50f4e3
Karl <ddtbone1>
2025-07-14 12:58:53 +0800

    initial commit - first try

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class PortfolioController : Controller
+{
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+}
